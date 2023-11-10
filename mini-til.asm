07c8876fff4bae9146bf13081572c073f6350668
David Detweiler <daviddetweiler1@gmail.com>
2023-11-10 13:20:05 -0800

    Add `da` macro

--- mini-til.asm
+++ mini-til.asm
@@ -9,7 +9,25 @@ global boot
     extern WriteFile
     extern ReadFile
     extern GetLastError
+
+    %define base_address 0
+
+    %macro call_import 1
+        call %1
+    %endmacro
+%else
+    %define base_address 0x2000000000
+
+    %macro call_import 1
+        call [%1]
+    %endmacro
 %endif
+
+%define address(label) (label) + base_address
+
+%macro da 1
+    dq address(%1)
+%endmacro
 
 %define tp r15
 %define wp r14
@@ -31,7 +49,7 @@ global boot
 
 %assign top_entry 0
 %define entry(id) header_ %+ id
-%define header_0 0
+%define header_0 0 - base_address
 %assign this_entry_flag 0
 %assign dict_finalized 0
 
@@ -57,7 +75,7 @@ global boot
     [section .rdata]
         align 8
         entry(%$this_entry):
-            dq entry(top_entry)
+            da entry(top_entry)
             db %$length | this_entry_flag
             db %$name, 0
 
@@ -82,14 +100,14 @@ global boot
     [section .rdata]
         align 8
         %1:
-            dq %2
+            da %2
 
     __?SECT?__
 %endmacro
 
 %macro shared 1
     section .rdata
-        constant shared_ %+ %1, %1
+        constant shared_ %+ %1, address(%1)
 
     section .text
         align 16
@@ -133,7 +151,7 @@ global boot
         %error "variable size must be at least 1"
     %endif
 
-    constant %1, %%storage
+    constant %1, address(%%storage)
     [section .bss]
         align 8
         %%storage:
@@ -142,27 +160,32 @@ global boot
     __?SECT?__
 %endmacro
 
-%macro maybe 1
-    dq maybe_impl
+%macro literal 1
+    da literal_impl
     dq %1
 %endmacro
 
+%macro maybe 1
+    da maybe_impl
+    da %1
+%endmacro
+
 %macro jump_to 1
-    dq jump_impl
-    dq %1 - %%following
+    da jump_impl
+    da %1 - %%following
     %%following:
 %endmacro
 
 %macro branch_to 1
-    dq branch_impl
-    dq %1 - %%following
+    da branch_impl
+    da %1 - %%following
     %%following:
 %endmacro
 
 %macro either_or 2
-    dq either_or_impl
-    dq %1
-    dq %2
+    da either_or_impl
+    da %1
+    da %2
 %endmacro
 
 %define config_input_buffer_size 4096
@@ -198,7 +221,7 @@ section .text
         next
 
     ; -- value
-    primitive literal
+    primitive literal_impl
         mov rax, [tp]
         add tp, 8
         sub dp, 8
@@ -208,7 +231,7 @@ section .text
     ; code --
     primitive exit_process
         mov rcx, [dp]
-        call ExitProcess
+        call_import ExitProcess
 
     ; -- constant
     shared impl_constant
@@ -235,7 +258,7 @@ section .text
     ; id -- handle?
     primitive get_std_handle
         mov rcx, [dp]
-        call GetStdHandle
+        call_import GetStdHandle
         cmp rax, -1
         je .invalid
         test rax, rax
@@ -256,7 +279,7 @@ section .text
         lea r9, [rsp + 8 * 5]
         xor rax, rax
         mov [rsp + 8 * 4], rax
-        call WriteFile
+        call_import WriteFile
         add dp, 8 * 2
         mov [dp], rax
         next
@@ -294,7 +317,7 @@ section .text
         lea r9, [rsp + 8 * 5]
         xor rax, rax
         mov [rsp + 8 * 4], rax
-        call ReadFile
+        call_import ReadFile
         add dp, 8
         mov [dp], rax
         mov rax, [rsp + 8 * 5]
@@ -373,7 +396,7 @@ section .text
 
     ; -- last-error
     primitive get_last_error
-        call GetLastError
+        call_import GetLastError
         sub dp, 8
         mov [dp], rax
         next
@@ -489,73 +512,71 @@ section .text
 section .rdata
     align 8
     program:
-        dq set_rstack
-        dq initialize
+        da set_rstack
+        da initialize
 
         .next_input:
-        dq parse
-        dq copy
-        dq eq_zero
+        da parse
+        da copy
+        da eq_zero
         maybe exit
-        dq print
+        da print
         jump_to .next_input
 
     ; --
     procedure initialize
-        dq set_dstack
-        dq init_io
-        dq kernel
-        dq dictionary
-        dq store
-        dq banner
-        dq print
-        dq return
+        da set_dstack
+        da init_io
+        da kernel
+        da dictionary
+        da store
+        da banner
+        da print
+        da return
 
     variable dictionary, 1
 
     ; --
     procedure exit
-        dq zero
-        dq exit_process
+        da zero
+        da exit_process
 
     variable stdin_handle, 1
     variable stdout_handle, 1
 
     ; --
     procedure init_io
-        dq literal
-        dq -10
-        dq get_std_handle
-        dq assert
-        dq stdin_handle
-        dq store
+        literal -10
+        da get_std_handle
+        da assert
+        da stdin_handle
+        da store
 
-        dq literal
-        dq -11
-        dq get_std_handle
-        dq assert
-        dq stdout_handle
-        dq store
+        literal -11
+        da get_std_handle
+        da assert
+        da stdout_handle
+        da store
 
-        dq input_buffer
-        dq copy
-        dq input_end_ptr
-        dq store
-        dq input_read_ptr
-        dq store
+        da input_buffer
+        da copy
+        da input_end_ptr
+        da store
+        da input_read_ptr
+        da store
 
-        dq return
+        da return
 
     string banner, `Mini-TIL (c) 2023 David Detweiler\n\n`
 
     ; ptr size --
     procedure print
-        dq stdout_handle
-        dq load
-        dq write_file
-        dq assert
-        dq drop
-        dq return
+        da stdout_handle
+        da load
+        da write_file
+        da assert
+        da drop
+        da return
 
     variable input_buffer, (config_input_buffer_size / 8) + 1
     variable input_end_ptr, 1
@@ -563,40 +584,39 @@ section .rdata
 
     ; -- eof?
     procedure input_refill
-        dq input_buffer
-        dq copy
-        dq input_read_ptr
-        dq store
-        dq input_buffer_size
-        dq stdin_handle
-        dq load
-        dq read_file
+        da input_buffer
+        da copy
+        da input_read_ptr
+        da store
+        da input_buffer_size
+        da stdin_handle
+        da load
+        da read_file
         branch_to .success
-        dq get_last_error
-        dq literal
-        dq 0x6d
-        dq eq
+        da get_last_error
+        literal 0x6d
+        da eq
         branch_to .success
-        dq crash
+        da crash
 
         .success:
-        dq copy
-        dq input_buffer
-        dq add
-        dq zero
-        dq over
-        dq store_byte
-        dq input_end_ptr
-        dq store
-        dq eq_zero
-        dq return
+        da copy
+        da input_buffer
+        da add
+        da zero
+        da over
+        da store_byte
+        da input_end_ptr
+        da store
+        da eq_zero
+        da return
 
     ; value -- value
     procedure assert
-        dq copy
-        dq eq_zero
+        da copy
+        da eq_zero
         maybe crash
-        dq return
+        da return
 
     constant zero, 0
     variable input_read_ptr, 1
@@ -605,59 +625,59 @@ section .rdata
     ;
     ; Signals EOF by returning length 0
     procedure parse
-        dq input_refill
+        da input_refill
         branch_to .eof
 
-        dq parse_buffer
-        dq parse_write_ptr
-        dq store
+        da parse_buffer
+        da parse_write_ptr
+        da store
         
-        dq parse_strip_spaces
-        dq copy
-        dq eq_zero
+        da parse_strip_spaces
+        da copy
+        da eq_zero
         branch_to .eof
 
-        dq input_end_ptr
-        dq load
-        dq over
-        dq sub
-        dq return
+        da input_end_ptr
+        da load
+        da over
+        da sub
+        da return
 
         .eof:
-        dq zero
-        dq copy
-        dq return
+        da zero
+        da copy
+        da return
 
     ; -- after-spaces-ptr?
     ;
     ; Signals EOF by returning null
     procedure parse_strip_spaces
         .again:
-        dq input_read_ptr
-        dq load
-        dq parse_consume_spaces
-        dq input_update
+        da input_read_ptr
+        da load
+        da parse_consume_spaces
+        da input_update
         branch_to .eof
         branch_to .again
-        dq input_read_ptr
-        dq load
-        dq return
+        da input_read_ptr
+        da load
+        da return
 
         .eof:
-        dq zero
-        dq return
+        da zero
+        da return
 
     ; read-ptr -- fresh-input? eof?
     procedure input_update
-        dq copy
-        dq input_read_ptr
-        dq store
-        dq input_end_ptr
-        dq load
-        dq eq
-        dq copy
+        da copy
+        da input_read_ptr
+        da store
+        da input_end_ptr
+        da load
+        da eq
+        da copy
         either_or input_refill, zero
-        dq return
+        da return
 
     variable parse_buffer, config_parse_buffer_size / 8
     variable parse_word_length, 1
